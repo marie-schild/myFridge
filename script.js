d05949250803eb33f64384eec9fa9c4922530d58
gits-projects-c <74082035+gits-projects-c@users.noreply.github.com>
2021-03-13 14:55:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,77 +41,9 @@ if (xhr.status === 200) {
 document.write(xhr + xhr.status);
 
 }
-/*
-// liefert das erste Element zurück mit der Klasse "result"
-var result = document.querySelector("result");
-
-var vorname = document.getElementById("vorname");
-// let vorname = document.querySelector('#vorname');
-
-var nachname = document.getElementById("nachname");
-// let nachname = document.querySelector('#nachname');
-
-var email = document.getElementById("email");
-// let email = document.querySelector('#email');
-
-var passwort = document.getElementById("passwort");
-// let passwort = document.querySelector('#passwort');
-
-// Creating a XHR object 
-var xhr = new XMLHttpRequest();
-var url = "https://myfridge-backend.herokuapp.com/api/registration";
-
-// open a connection
-xhr.open("POST", url, true);
-
-// Set the request header i.e. which type of content you are sending 
-xhr.setRequestHeader("Content-Type", "application/json");
-
-// Create a state change callback 
-xhr.onreadystatechange = function () { 
-  if (xhr.readyState === 400 && xhr.status === 200) { 
-        // Print received data from server 
-        result.innerHTML = xhr.responseText;       
-    } 
-}; 
-
-// JSON data
-var data = { 
-  "vorname": vorname.value, 
-  "nachname": nachname.value, 
-  "email": email.value,
-  "passwort": passwort.value }; 
 
 
-var data = { 
-  vorname: vorname.value, 
-  nachname: nachname.value, 
-  email: email.value,
-  passwort: passwort.value };
 
-
-// Sending data with the request 
-//var json = JSON.stringify(data);
-xhr.send(data);
-
-//result.innerHTML
-
-
-// Erster Versuch:
-//  var vorname = document.getElementById("vorname");
-//  var nachname = document.getElementById("nachname");
-//  var passwort = document.getElementById("passwort");
-//  var email = document.getElementById("email");
-
-}
-
-//$(document).ready(function () {
-//  $('#loginModal').modal('show');
-//  $(function () {
-//    $('[data-toggle="tooltip"]').tooltip()
-//  })
-//});
-*/
 
 function loginForm() {
   
@@ -126,24 +58,28 @@ function loginForm() {
   xhr.open("POST", url);
   
   xhr.setRequestHeader("Content-Type", "application/json");
+    
+    
+  var data = JSON.stringify({
+    "email" : email1.value,
+    "passwort" : passwort1.value
+  });
+  
+  xhr.send(data);
   
   xhr.onreadystatechange = function () {
      if (xhr.readyState === 4) {
         console.log(xhr.status);
         console.log(xhr.responseText);
-     }};
-  
-  var data = JSON.stringify({
-    "email" : email1.value,
-    "passwort" : passwort1.value
-});
-  
-  xhr.send(data);
-  
-  if (xhr.status === 200) {
-    alert("Sie haben sich als"+ vorname.value + " " + nachname.value + "registriert. Sie können sich nun einloggen");
-  }
+        if (xhr.status === 200) {
+          alert("Vielen Dank fürs Einloggen!");
+          window.location = "https://Kuhlschrankverwaltung.chrisfitzer.repl.co/MeinBereich.html";
+        } else {
+          var false_l = document.getElementById("false-login");
 
-  document.write(data + xhr + xhr.status);
-  
-  }
+          false_l.innerHTML("Email oder Passwort ist falsch");
+          alert("Falsch angemeldet");
+        }
+     }};
+
+}
