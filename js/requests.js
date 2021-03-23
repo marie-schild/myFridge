4cf81bf650ec534fe20f3f5ce44eec47871f07be
marie-schild <80281929+marie-schild@users.noreply.github.com>
2021-03-23 14:02:12 +0100

    Update requests.js

--- js/requests.js
+++ js/requests.js
@@ -1,56 +1,66 @@
+// Deklaration der Konstante "url", welche als Base-URL verwendet wird
 const url = "https://myfridge-backend.herokuapp.com/api/"
 
+// Asynchrone Funktion die Daten abruft und dazu einen Endpoint und einen Header entgegen nimmt
 async function getData(endpoint = '', headers = {}) {
-    const response = await fetch(url + endpoint, {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers:  headers,
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer'
-    });
-    return response.json();
+  // Deklaration der Konstante "response", die die Datenabfrage abwartet
+  const response = await fetch(url + endpoint, {
+    // Definieren dass Daten abgerufen werden
+    method: 'GET',
+    
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers:  headers,
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  });
+  return response.json();
 }
 
+// Asynchrone Funktion die Daten erstellt und dazu einen Endpoint, die Daten und einen Header entgegen nimmt
 async function postData(endpoint = '', data = {}, headers = {}) {
-        const response = await fetch(url + endpoint, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers:  headers,
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
-            body: JSON.stringify(data)
-        })
-        return response.json();    
+  const response = await fetch(url + endpoint, {
+    // Definieren dass Daten erstellt werden
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers:  headers,
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data)
+  })
+  return response.json();    
 }
 
+// Asynchrone Funktion die Daten aktualisiert und dazu einen Endpoint, die Daten und einen Header entgegen nimmt
 async function putData(endpoint = '', data = {}, headers = {}) {
-    const response = await fetch(url + endpoint, {
-        method: 'PUT',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers:  headers,
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    });
-    return response.json();
+  const response = await fetch(url + endpoint, {
+    method: 'PUT',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers:  headers,
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data)
+  });
+  return response.json();
 }
 
+// Asynchrone Funktion die Daten löscht und dazu einen Endpoint und einen Header entgegen nimmt
 async function deleteData(endpoint = '', headers = {}) {
-    const response = await fetch(url + endpoint, {
-        method: 'DELETE',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers:  headers,
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    });
-    return response.json();
+  const response = await fetch(url + endpoint, {
+    // Definieren dass Daten gelöscht werden
+    method: 'DELETE',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers:  headers,
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+    //body: JSON.stringify(data)
+  });
+  return response.json();
 }
